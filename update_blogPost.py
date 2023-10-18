8b69f92656ec3ad3366b994894501430e46f2c2c
teawon <78795820+teawon@users.noreply.github.com>
2023-10-18 19:42:55 +0900

    readme 최근 블로그 글 개수 수정

--- update_blogPost.py
+++ update_blogPost.py
@@ -3,12 +3,12 @@ import feedparser
 blog_url = "https://teawon.github.io/feed.xml"
 rss_feed = feedparser.parse(blog_url)
 
-MAX_NUM = 4
+MAX_NUM = 5
 
 latest_posts = ""
 
 for idx, entrie in enumerate(rss_feed['entries']):
-  if idx > MAX_NUM:
+  if idx >= MAX_NUM:
      break;
   feed_date = entrie['published_parsed']
   latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
